d9b96809b28356e4e47cdf3dc065a8e9d8482f67
Mattpenguin <mattpenguinmodding@gmail.com>
2019-10-22 19:26:14 -0500

    Add basic files, modify resource configs

---
+++ src/main/java/com/mattpenguin/metalus/Metalus.java
@@ -0,0 +1,9 @@
+package com.mattpenguin.metalus;
+
+import com.mattpenguin.metalus.common.Constant;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constant.MOD_ID)
+public class Metalus {
+
+}
---
+++ src/main/java/com/mattpenguin/metalus/common/Constant.java
@@ -0,0 +1,5 @@
+package com.mattpenguin.metalus.common;
+
+public class Constant {
+    public static final String MOD_ID = "metalus";
+}
